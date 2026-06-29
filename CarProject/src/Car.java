5e45ecbd9d6fc92cd6de4686f4402fa267aee5b3
HeitorQueiroz2008 <heitorqueirozpro@gmail.com>
2026-06-29 12:53:38 -0300

    refactor: unnecessary setters removed

--- CarProject/src/Car.java
+++ CarProject/src/Car.java
@@ -169,17 +169,9 @@ public class Car {
     public int getGear() {
         return gear;
     }
-
-    public void setGear(int gear) {
-        this.gear = gear;
-    }
-
+    
     public int getSpeed() {
         return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
     }
 
     //builder
