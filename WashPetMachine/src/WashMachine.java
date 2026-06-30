0f358e14bf807e6d9f9fb6a1d1fa150cfe67cbee
HeitorQueiroz2008 <heitorqueirozpro@gmail.com>
2026-06-30 10:59:52 -0300

    feat: variables and builder

--- WashPetMachine/src/WashMachine.java
+++ WashPetMachine/src/WashMachine.java
@@ -1,4 +1,19 @@
 public class WashMachine {
 
+    //Variáveis
+    private boolean isFull;
 
+    private boolean isDirt;
+
+    private int waterLevel;
+
+    private int shampooLevel;
+
+    //Builder
+    public WashMachine(){
+        isDirt = false;
+        isFull = false;
+        waterLevel = 15;
+        shampooLevel = 5;
+    }
 }
