524183468ce9396fd720b1eb68ac5ee2fe8e8436
HeitorQueiroz2008 <heitorqueirozpro@gmail.com>
2026-07-02 11:07:15 -0300

    fix: giveWash validations and values, reffils validations

--- WashPetMachine/src/WashMachine.java
+++ WashPetMachine/src/WashMachine.java
@@ -1,89 +1,98 @@
-import java.util.Scanner;
-
 public class WashMachine {
 
     //Variáveis
-    Scanner scanner = new Scanner(System.in);
 
     private boolean isFull;
 
     private boolean isDirt;
+
+    private boolean petDirt;
 
     private int waterLevel;
 
     private int shampooLevel;
 
     //Métodos
-    public void addPet(){
-        if (!isFull && !isDirt){
+    public void addPet() {
+        if (!isFull && !isDirt) {
             isFull = true;
             isDirt = true;
-            System.out.println("O pet foi adicionado à máquina!");
-        } else if (isFull){
-            System.out.println("A máquina já está ocupada!");
+            petDirt = true;
+            System.out.println("\nO pet foi adicionado à máquina!\n");
+        } else if (isFull) {
+            System.out.println("\nA máquina já está ocupada!\n");
         } else {
-            System.out.println("A máquina está suja!\nlimpe-a para usar");
+            System.out.println("\nA máquina está suja!\nlimpe-a para usar\n");
         }
     }
 
-    public void takeOffPet(){
-        if (isFull){
+    public void removePet() {
+        if (isFull) {
             isFull = false;
-            System.out.println("O pet foi retirado da máquina!");
-            if (isDirt){
-                System.out.println("Você sujou a máquina!\nLimpe-a para continuar usando...");
+            System.out.println("\nO pet foi retirado da máquina!\n");
+            if (isDirt) {
+                System.out.println("\nVocê sujou a máquina!\nLimpe-a para continuar usando...\n");
             }
         } else {
-            System.out.println("A máquina já está vazia!");
+            System.out.println("\nA máquina já está vazia!\n");
         }
     }
 
-    public void cleanMachine(){
+    public void cleanMachine() {
         if (isDirt) {
-            if (waterLevel >= 3 && shampooLevel >= 1){
+            if (waterLevel >= 3 && shampooLevel >= 1) {
                 waterLevel -= 3;
                 shampooLevel -= 1;
                 isDirt = false;
-                System.out.printf("A máquina foi limpa!\nNível atual de água: %s L\nNível atual de shampoo: %s L\n", waterLevel, shampooLevel);
+                System.out.printf("\nA máquina foi limpa!\nNível atual de água: %s L\nNível atual de shampoo: %s L\n\n", waterLevel, shampooLevel);
             } else {
-                System.out.printf("Sem recursos suficiente. Abasteça!\\nNível atual de água: %s L\\nNível atual de shampoo: %s L\n", waterLevel, shampooLevel);
+                System.out.printf("\nSem recursos suficiente. Abasteça!\\nNível atual de água: %s L\\nNível atual de shampoo: %s L\n", waterLevel, shampooLevel);
             }
         } else {
-            System.out.println("A máquina já está limpa!");
+            System.out.println("\nA máquina já está limpa!\n");
         }
     }
 
-    public void giveWash(){
-        if (waterLevel >= 10 && shampooLevel >= 2){
-            waterLevel -= 10;
-            shampooLevel -= 10;
-            isDirt = false;
-            System.out.println("Pet limpo com sucesso! Retire-o da máquina");
+    public void giveWash() {
+        if (isFull && petDirt) {
+            if (waterLevel >= 10 && shampooLevel >= 2) {
+                waterLevel -= 10;
+                shampooLevel -= 2;
+                isDirt = false;
+                petDirt = false;
+                System.out.println("\nPet limpo com sucesso! Retire-o da máquina\n");
+            }
+        }
+        if (!petDirt) {
+            System.out.println("\nO pet já foi lavado! Retire-o da máquina para prosseguir!\n");
+        } else if (!isFull) {
+            System.out.println("\nA máquina está vazia! Insira um pet para lavá-lo\n");
         } else {
-            System.out.println("Recursos insuficientes! Verifique os níveis de água e shampoo para continuar com a limpeza...");
-            System.out.printf("Nível de água: %s L \nNível de shampoo: %s L \n", getWaterLevel(), getShampooLevel());
+            System.out.println("\nRecursos insuficientes! Verifique os níveis de água e shampoo para continuar com a limpeza...");
+            System.out.printf("\nNível de água: %s L \nNível de shampoo: %s L \n\n", getWaterLevel(), getShampooLevel());
         }
+
     }
 
-    public void refillWater (){
+    public void refillWater() {
 
-        if ((waterLevel) < 29) {
+        if ((waterLevel) < 28) {
             waterLevel += 2;
-            System.out.printf("Água abastecida! \nNível atual: %s L\n", waterLevel);
+            System.out.printf("\nÁgua abastecida! \nNível atual: %s L\n", waterLevel);
         } else {
             waterLevel = 30;
-            System.out.println("Limite máximo de água alcançado!\nNível atual: 30 L");
+            System.out.println("\nLimite máximo de água alcançado!\nNível atual: 30 L\n");
         }
     }
 
-    public void refillShampoo(){
+    public void refillShampoo() {
 
-        if ((shampooLevel) < 9) {
+        if ((shampooLevel) < 8) {
             shampooLevel += 2;
-            System.out.printf("Shampoo abastecido! \nNível atual: %s L\n", shampooLevel);
+            System.out.printf("\nShampoo abastecido! \nNível atual: %s L\n", shampooLevel);
         } else {
             shampooLevel = 10;
-            System.out.println("Limite máximo de shampoo alcançado!\nNível atual: 10 L");
+            System.out.println("\nLimite máximo de shampoo alcançado!\nNível atual: 10 L\n");
         }
     }
 
@@ -98,8 +107,12 @@ public class WashMachine {
         return shampooLevel;
     }
 
+    public boolean isFull() {
+        return isFull;
+    }
+
     //Builder
-    public WashMachine(){
+    public WashMachine() {
         isDirt = false;
         isFull = false;
         waterLevel = 15;
