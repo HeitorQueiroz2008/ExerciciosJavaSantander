7dabbbb949a0c2eb5ea2172dcbce8402f71d7458
HeitorQueiroz2008 <heitorqueirozpro@gmail.com>
2026-07-23 10:45:09 -0300

    refactor: better menu style

--- HerancaPolimorfismo/Cinema/src/Main.java
+++ HerancaPolimorfismo/Cinema/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
 
-        System.out.println("---INGRESSOS DE CINEMA---\n");
+        System.out.println("---INGRESSOS DE CINEMA---");
 
         //Entrada
         while (true) {
             Ticket ticket = null;
-            System.out.println("Escolha o tipo de ingresso: \n[1]Comum\n[2]Meia-entrada\n[3]Família\n[4]Encerrar\n");
+            System.out.println("\nEscolha o tipo de ingresso: \n[1]Comum\n[2]Meia-entrada\n[3]Família\n[4]Encerrar\n");
             var choice = scanner.nextInt();
 
             if (choice == 1){
@@ -27,11 +27,11 @@ public class Main {
             }
 
 
-            System.out.println("Insira o nome do filme:");
+            System.out.println("\nInsira o nome do filme:");
             ticket.setMovieName(scanner.next());
 
             while (true) {
-                System.out.println("Dublado [1] ou Legendado [2]?:");
+                System.out.println("\nDublado [1] ou Legendado [2]?:");
                 choice = scanner.nextInt();
                 if (choice == 1){
                     ticket.setDubbed(true);
@@ -44,24 +44,25 @@ public class Main {
                 }
             }
 
-            System.out.println("Insira o valor da entrada:");
+            System.out.println("\nInsira o valor da entrada:");
 
             ticket.setBaseValue(scanner.nextDouble());
 
             if (ticket instanceof FamilyTicket){
-                System.out.println("Insira a quantidade de pessoas: ");
+                System.out.println("\nInsira a quantidade de pessoas: ");
                 ticket.setPeople(scanner.nextInt());
             }
             //Processamento
             ticket.calculateFinalValue();
 
             //Saída
+            System.out.println("\nImprimindo ingresso...\n");
             System.out.println("*****INGRESSO*****");
             System.out.printf("Nome do filme: %s\n", ticket.getMovieName());
             if (ticket.isDubbed()){
                 System.out.println("Tipo de exibição: Dublado");
             } else {
-                System.out.println("Tipo de exibição: legendado");
+                System.out.println("Tipo de exibição: Legendado");
             }
             System.out.printf("Número de entradas: %s\n", ticket.getPeople());
             System.out.printf("Valor base do ingresso: R$ %s\n", ticket.getBaseValue());
@@ -70,7 +71,7 @@ public class Main {
             } else if (ticket.getPeople() > 3){
                 System.out.println("Você obteve 5% de desconto no valor total!!!");
             }
-            System.out.printf("Valor total: R$ %s", ticket.getFinalValue());
+            System.out.printf("Valor total: R$ %s\n", ticket.getFinalValue());
             System.out.println("******************");
             break;
         }
